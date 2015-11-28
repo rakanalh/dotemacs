9208668d0d175d2fb0d234a06998e9a9be851deb
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2015-11-28 23:28:07 +0100

    Working copy

--- init.el
+++ init.el
@@ -22,13 +22,43 @@
  )
 
 (require 'powerline)
+(require 'helm)
 (require 'helm-config)
 (require 'jedi)
+(require 'sr-speedbar)
 
+(global-linum-mode nil)
+(setq sr-speedbar-right-side nil)
 (powerline-default-theme)
 (projectile-global-mode)
-(helm-mode 1)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(helm-mode 1)
 
+(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
 (add-to-list 'ac-sources 'ac-source-jedi-direct)
+
 (add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'pip-requirements-mode-hook #'pip-requirements-auto-complete-setup)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Custom line number stuff
+(setq linum-format "%d  ")
+(setq-default left-fringe-width  12)
+(setq-default right-fringe-width  0)
+(set-face-attribute 'fringe nil)
+
+
+;; COPY / PASTE on OSX
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
