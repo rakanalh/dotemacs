787c48e1267fd13cf855dc6bdab11da40548c68d
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2015-11-30 23:55:01 +0100

    Automatically invoke goimports and gofmt on save

--- init.el
+++ init.el
@@ -109,9 +109,23 @@
 
 ;; Custom Shortcuts
 (global-set-key (kbd "C-}")
-    (lambda () (interactive) (forward-line 5)))
+                (lambda () (interactive) (forward-line 5)))
 (global-set-key (kbd "C-{")
-    (lambda () (interactive) (forward-line -5)))
+                (lambda () (interactive) (forward-line -5)))
+
+;; Go Stuff
+(defun my-go-mode-hook ()
+  ; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
+  ; Call Gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  ; Godef jump key binding
+  (local-set-key (kbd "M-.") 'godef-jump))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (require 'spacemacs-startup)
 (spacemacs/setup-startup-hook)
