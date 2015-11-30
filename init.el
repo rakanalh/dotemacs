83bfd208a022d374ffd7845d88b24f4b2c1f95f8
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2015-11-30 22:07:02 +0100

    Added autopep8 along with some better defaults

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'load-path "~/.emacs.d/vendor/")
 (package-initialize)
 
 (custom-set-variables
@@ -23,8 +24,8 @@
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work
-)
+ ;; If there is more than one, they won't work right.
+ '(widget-button ((t (:foreground "blue" :underline nil :weight bold)))))
 
 (require 'helm)
 (require 'helm-config)
@@ -33,6 +34,15 @@
 (require 'sr-speedbar)
 (require 'spaceline-config)
 (require 'which-key)
+(require 'py-autopep8)
+
+;; Disable toolbar & menubar
+(menu-bar-mode -1)
+  (when (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))
+  (when (fboundp 'scroll-bar-mode)
+    (scroll-bar-mode -1))
+
 
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
 (global-git-gutter+-mode)
@@ -42,12 +52,23 @@
 (projectile-global-mode)
 (recentf-mode 1)
 (spaceline-emacs-theme)
+(setq inhibit-startup-message t)
 (setq sr-speedbar-right-side nil)
 (setq make-backup-files nil)
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
+(setq-default indent-tabs-mode nil)
+(setq x-select-enable-clipboard t
+      save-interprogram-paste-before-kill t
+      mouse-yank-at-point t
+      require-final-newline t
+      visible-bell t
+      load-prefer-newer t
+      backup-directory-alist `(("." . ,(concat user-emacs-directory
+					       "backups"))))
+(show-paren-mode 1)
 (which-key-mode)
-
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
 (add-to-list 'ac-sources 'ac-source-jedi-direct)
@@ -81,3 +102,9 @@
     (lambda () (interactive) (forward-line 5)))
 (global-set-key (kbd "C-{")
     (lambda () (interactive) (forward-line -5)))
+
+(require 'spacemacs-startup)
+(spacemacs/setup-startup-hook)
+
+(provide 'init)
+;;; init.el ends here
