a86842d1a63a8b6221301dff11c3b6688f600a1b
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-09-16 10:20:34 +0200

    Added magit shortcuts

--- extensions.el
+++ extensions.el
@@ -93,9 +93,12 @@
   :bind
   ;; Magic
   ("C-x g s" . magit-status)
+  ("C-x g x" . magit-checkout)
   ("C-x g c" . magit-commit)
   ("C-x g p" . magit-push)
-  ("C-x g u" . magit-pull))
+  ("C-x g u" . magit-pull)
+  ("C-x g e" . magit-ediff-resolve)
+  ("C-x g r" . magit-rebase-interactive))
 
 (use-package magit-popup)
 
