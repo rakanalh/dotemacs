232d32615f8fcd244c2358e10f40b00fe6c1dbab
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-10-30 19:25:51 +0100

    Init packages in core

--- core/core.el
+++ core/core.el
@@ -1,3 +1,14 @@
+(package-initialize)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/")
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
 (defconst private-dir  (expand-file-name "private" user-emacs-directory))
 (defconst temp-dir
   (format "%s/cache" private-dir)
--- init.el
+++ init.el
@@ -9,21 +9,17 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
 
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/")
-             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
 (add-to-list 'load-path (concat user-emacs-directory "core"))
 (add-to-list 'load-path "~/.emacs.d/vendor/")
 (add-to-list 'load-path "~/.emacs.d/vendor/doom-theme")
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
 
 (require 'core)
 (require 'core-functions)
