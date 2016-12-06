d58932dea8d72bb8f37b8608a61150876ac5b31a
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-12-07 00:06:02 +0100

    Activate C modes for C stuff

--- core/core-c.el
+++ core/core-c.el
@@ -18,15 +18,17 @@
 
 (use-package semantic
   :config
-  (global-semanticdb-minor-mode 1)
-  (global-semantic-idle-scheduler-mode 1)
-  (global-semantic-stickyfunc-mode 1)
-  (semantic-mode 1))
+  (add-hook 'c-mode-common-hook (lambda ()
+	    (semantic-mode 1)
+	    ;(global-semanticdb-minor-mode 1)
+	    (global-semantic-idle-scheduler-mode 1)
+	    (global-semantic-stickyfunc-mode 1))))
 
 (use-package ede
   :config
   ;; Enable EDE only in C/C++
-  (global-ede-mode))
+  (add-hook 'c-mode-common-hook (lambda ()
+				  (global-ede-mode))))
 
 (use-package ggtags
   :config
@@ -35,7 +37,6 @@
                                ":"
                                "/usr/local/include"))
   (setenv "MAKEOBJDIRPREFIX" (file-truename "~/.obj/"))
-  (ggtags-mode 1)
   (add-hook 'c-mode-common-hook
 	    (lambda ()
 	      (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
@@ -58,32 +59,14 @@
   :init
   (add-to-list 'company-backends 'company-c-headers))
 
-(use-package cc-mode
-  :init
-  (define-key c-mode-map  [(tab)] 'company-complete)
-  (define-key c++-mode-map  [(tab)] 'company-complete))
-
-;; git@github.com:syohex/emacs-counsel-gtags.git
-(use-package counsel-gtags
-  :ensure t
-  :config
-  (add-hook 'c-mode-hook 'counsel-gtags-mode)
-  (add-hook 'c++-mode-hook counsel-gtags-mode)
-
-  (with-eval-after-load 'counsel-gtags
-    (define-key counsel-gtags-mode-map (kbd "M-t") 'counsel-gtags-find-definition)
-    ;(define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
-    ;(define-key counsel-gtags-mode-map (kbd "M-s") 'counsel-gtags-find-symbol)
-    (define-key counsel-gtags-mode-map (kbd "M-,") 'counsel-gtags-pop-stack)))
-
-(defun alexott/cedet-hook ()
+(defun enable-semantic-shortcuts ()
   (local-set-key (kbd "C-c C-j") 'semantic-ia-fast-jump)
   (local-set-key (kbd "C-c C-s") 'semantic-ia-show-summary))
 
 ;; hs-minor-mode for folding source code
 (add-hook 'c-mode-common-hook 'hs-minor-mode)
-(add-hook 'c-mode-common-hook 'alexott/cedet-hook)
-(add-hook 'c-mode-hook 'alexott/cedet-hook)
+(add-hook 'c-mode-common-hook 'enable-semantic-shortcuts)
+(add-hook 'c-mode-hook 'enable-semantic-shortcuts)
 (add-hook 'c++-mode-hook 'alexott/cedet-hook)
 
 (provide 'core-c)
