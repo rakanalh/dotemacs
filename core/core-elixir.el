c683e79591951f491e2b84f6f3f19233b0adaf0d
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-12-28 23:51:42 +0100

    Added Elixir

---
+++ core/core-elixir.el
@@ -0,0 +1,12 @@
+(use-package alchemist
+  :config
+  (add-hook 'elixir-mode-hook 'alchemist-mode))
+
+(use-package flycheck-elixir)
+
+(use-package flycheck-mix
+  :commands (flycheck-mix-setup))
+
+(use-package elixir-mode)
+
+(provide 'core-elixir)
