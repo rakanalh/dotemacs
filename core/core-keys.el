0f91f545c8a087fbc8d7f28ae5fe46f47bad19a7
Rakan Alhneiti <ralhneiti@newstore.com>
2017-10-06 16:48:32 +0200

    Replace binding to move 5 lines a stroke

--- core/core-keys.el
+++ core/core-keys.el
@@ -14,11 +14,14 @@
 (global-set-key (kbd "C-x C-b") 'switch-to-buffer)
 
 ;; Custom Emacs Shortcuts
+;; Move more quickly
+(global-set-key (kbd "C-S-n") (lambda () (interactive) (ignore-errors (forward-line 5))))
+(global-set-key (kbd "C-S-p") (lambda () (interactive) (ignore-errors (forward-line -5))))
+(global-set-key (kbd "C-S-f") (lambda () (interactive) (ignore-errors (forward-char 5))))
+(global-set-key (kbd "C-S-b") (lambda () (interactive) (ignore-errors (backward-char 5))))
 (global-set-key (kbd "M-<") (lambda () (interactive) (beginning-of-buffer-record)))
 (global-set-key (kbd "M->") (lambda () (interactive) (end-of-buffer-record)))
 (global-set-key (kbd "M-/") (lambda () (interactive) (go-back-to-point)))
-(global-set-key (kbd "C-}") (lambda () (interactive) (forward-line 5)))
-(global-set-key (kbd "C-{") (lambda () (interactive) (forward-line -5)))
 (global-set-key (kbd "M-i") (lambda () (interactive) (codenav-prev-definition)))
 (global-set-key (kbd "M-k") (lambda () (interactive) (codenav-next-definition)))
 
