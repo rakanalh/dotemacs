f7c1924b432a9dceb6e1121720abe3267be014e4
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2018-09-15 16:29:22 +0200

    Ivy rich mode

--- core/core-ivy.el
+++ core/core-ivy.el
@@ -15,13 +15,8 @@
 
 (use-package ivy-rich
   :after ivy
-  :custom
-  (ivy-virtual-abbreviate 'full
-                          ivy-rich-switch-buffer-align-virtual-buffer t
-                          ivy-rich-path-style 'abbrev)
   :config
-  (ivy-set-display-transformer 'ivy-switch-buffer
-                               'ivy-rich-switch-buffer-transformer))
+  (ivy-rich-mode 1))
 
 (use-package ivy-hydra)
 
